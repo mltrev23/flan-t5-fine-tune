80cf22c845412b838fa44819a5d2df138045f09f
mltrev23 <trevor.dev23@gmail.com>
2024-04-22 13:47:22 -0500

    updated

--- fine-tune-all.py
+++ fine-tune-all.py
@@ -92,3 +92,21 @@ trainer = Seq2SeqTrainer(
 )
 
 trainer.train()
+
+# Inference
+
+last_checkpoint = "./results/checkpoint-22500"
+
+finetuned_model = T5ForConditionalGeneration.from_pretrained(last_checkpoint)
+tokenizer = T5Tokenizer.from_pretrained(last_checkpoint)
+
+my_question = "What do you think about the benefit of Artificial Intelligence?"
+inputs = "Please answer to this question: " + my_question
+
+inputs = tokenizer(inputs, return_tensors="pt")
+outputs = finetuned_model.generate(**inputs)
+answer = tokenizer.decode(outputs[0])
+from textwrap import fill
+
+print(fill(answer, width=80))
+
