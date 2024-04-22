1a3f1281c64769d57b3b2dc60f6b17c15245b768
mltrev23 <trevor.dev23@gmail.com>
2024-04-22 13:46:15 -0500

    full fine-tune

---
+++ fine-tune-all.py
@@ -0,0 +1,94 @@
+import nltk
+import evaluate
+import numpy as np
+from datasets import load_dataset
+from transformers import T5Tokenizer, DataCollatorForSeq2Seq
+from transformers import T5ForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+# Load the tokenizer, model, and data collator
+MODEL_NAME = "google/flan-t5-base"
+
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+
+# Acquire the training data from Hugging Face
+DATA_NAME = "yahoo_answers_qa"
+yahoo_answers_qa = load_dataset(DATA_NAME)
+
+yahoo_answers_qa = yahoo_answers_qa["train"].train_test_split(test_size=0.3)
+
+# We prefix our tasks with "answer the question"
+prefix = "Please answer this question: "
+
+# Define the preprocessing function
+
+def preprocess_function(examples):
+   """Add prefix to the sentences, tokenize the text, and set the labels"""
+   # The "inputs" are the tokenized answer:
+   inputs = [prefix + doc for doc in examples["question"]]
+   model_inputs = tokenizer(inputs, max_length=128, truncation=True)
+  
+   # The "labels" are the tokenized outputs:
+   labels = tokenizer(text_target=examples["answer"], 
+                      max_length=512,         
+                      truncation=True)
+
+   model_inputs["labels"] = labels["input_ids"]
+   return model_inputs
+
+# Map the preprocessing function across our dataset
+tokenized_dataset = yahoo_answers_qa.map(preprocess_function, batched=True)
+
+nltk.download("punkt", quiet=True)
+metric = evaluate.load("rouge")
+
+def compute_metrics(eval_preds):
+   preds, labels = eval_preds
+
+   # decode preds and labels
+   labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+   decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+   decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+   # rougeLSum expects newline after each sentence
+   decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip())) for pred in decoded_preds]
+   decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) for label in decoded_labels]
+
+   result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+  
+   return result
+
+# Global Parameters
+L_RATE = 3e-4
+BATCH_SIZE = 8
+PER_DEVICE_EVAL_BATCH = 4
+WEIGHT_DECAY = 0.01
+SAVE_TOTAL_LIM = 3
+NUM_EPOCHS = 3
+
+# Set up training arguments
+training_args = Seq2SeqTrainingArguments(
+   output_dir="./results",
+   evaluation_strategy="epoch",
+   learning_rate=L_RATE,
+   per_device_train_batch_size=BATCH_SIZE,
+   per_device_eval_batch_size=PER_DEVICE_EVAL_BATCH,
+   weight_decay=WEIGHT_DECAY,
+   save_total_limit=SAVE_TOTAL_LIM,
+   num_train_epochs=NUM_EPOCHS,
+   predict_with_generate=True,
+   push_to_hub=False
+)
+
+trainer = Seq2SeqTrainer(
+   model=model,
+   args=training_args,
+   train_dataset=tokenized_dataset["train"],
+   eval_dataset=tokenized_dataset["test"],
+   tokenizer=tokenizer,
+   data_collator=data_collator,
+   compute_metrics=compute_metrics
+)
+
+trainer.train()
