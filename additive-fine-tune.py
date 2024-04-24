62a65978052a64f3405d2903fc046c1b1d89c225
mltrev23 <trevor.dev23@gmail.com>
2024-04-23 21:58:09 -0500

    updated

--- additive-fine-tune.py
+++ additive-fine-tune.py
@@ -109,11 +109,13 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-"""
 
+        
 input_text = "What is apple?"
 # Encode the input text to tensor
 input_ids = tokenizer.encode(input_text, return_tensors='pt').to('cuda')
+"""
+
 # Generate text using the model. Adjust the max_length as needed.
 #output_ids = model.generate(input_ids, max_length=100, num_beams=5, early_stopping=True)
 with torch.no_grad():
@@ -131,11 +133,19 @@ print("Input Text:", input_text)
 print("Generated Text:", generated_text)
 """
 outputs = input_ids
-max_length = 50  # Define the maximum length of the sequence
+max_length = 100  # Define the maximum length of the sequence
+
+decoder_start_token = tokenizer.pad_token_id
+decoder_input_ids = torch.full(
+    (input_ids.shape[0], 1), 
+    decoder_start_token, 
+    dtype=torch.long
+).to('cuda')
+
 with torch.no_grad():
     for _ in range(max_length):
         # Forward pass
-        logits = model(outputs).logits
+        logits = model.t5model(outputs, decoder_input_ids=decoder_input_ids).logits
 
         # Get the logits of the last predicted token in the output
         next_token_logits = logits[:, -1, :]
@@ -144,11 +154,12 @@ with torch.no_grad():
         next_token = torch.argmax(next_token_logits, dim=-1, keepdim=True)
 
         # Check if the last token is EOS (end of sentence) token
-        if next_token.item() == tokenizer.eos_token_id:
-            break
+        #if next_token.item() == tokenizer.eos_token_id:
+        #    break
 
         # Append the predicted token to the output sequence
         outputs = torch.cat([outputs, next_token], dim=1)
-
-generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        decoder_input_ids = torch.cat([decoder_input_ids, next_token], dim=1)
+print(decoder_input_ids[0])
+generated_text = tokenizer.decode(decoder_input_ids[0], skip_special_tokens=True)
 print(generated_text)
