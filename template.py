3220219e8a3acba639eb9d99dd99f8f573b6b967
mltrev23 <trevor.dev23@gmail.com>
2024-04-24 12:36:31 -0500

    updated

--- template.py
+++ template.py
@@ -14,7 +14,7 @@ for layer in model.decoder.block[start_index:]:
     for param in layer.parameters():
         param.requires_grad = True
 """
-from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling, Seq2SeqTrainer, Seq2SeqTrainingArguments
 
 # Load unlabeled data and tokenize it
 #unlabeled_data = open('bittensor.txt', 'r').readlines()
@@ -23,27 +23,36 @@ from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling,
 # Create training dataset for MLM
 dataset = LineByLineTextDataset(tokenizer=tokenizer, file_path='bittensor.txt', block_size=128)
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False, mlm_probability=0.15)
+# Global Parameters
+L_RATE = 3e-4
+BATCH_SIZE = 8
+PER_DEVICE_EVAL_BATCH = 4
+WEIGHT_DECAY = 0.01
+SAVE_TOTAL_LIM = 3
+NUM_EPOCHS = 30
 
-# Define training arguments
-training_args = TrainingArguments(
-    output_dir='./results',
-    overwrite_output_dir=True,
-    num_train_epochs=30,
-    per_device_train_batch_size=4,
-    save_steps=10_000,
-    save_total_limit=2,
+# Set up training arguments
+training_args = Seq2SeqTrainingArguments(
+   output_dir="./results",
+   learning_rate=L_RATE,
+   per_device_train_batch_size=BATCH_SIZE,
+   per_device_eval_batch_size=PER_DEVICE_EVAL_BATCH,
+   weight_decay=WEIGHT_DECAY,
+   save_total_limit=SAVE_TOTAL_LIM,
+   num_train_epochs=NUM_EPOCHS,
+   predict_with_generate=True,
+   push_to_hub=False
 )
 
-# Create Trainer and train the model
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=dataset,
+trainer = Seq2SeqTrainer(
+   model=model,
+   args=training_args,
+   train_dataset=dataset,
+   tokenizer=tokenizer,
+   data_collator=data_collator,
 )
 
 trainer.train()
-
 
 input_text = "What is bittensor?"
 # Encode the input text to tensor
