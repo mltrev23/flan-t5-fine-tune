9717295da972739dbe1d5887cb46a8620658dd29
Rene.Bustamante <rene.bustamante@chain-insights.ai>
2024-07-17 20:23:34 -0500

    feat: inference test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
 model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device_map="auto")
 
-input_text = ["What is your name?"]
+input_text = ["What is FAANG?"]
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
 print("---------------------", input_ids)
 
